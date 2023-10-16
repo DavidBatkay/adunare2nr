b42c9610d081269b8f59490ccc75612c26fb0011
DavidBatkay <david.batkay@emanuel.ro>
2023-10-16 22:10:02 +0300

    too easy

---
+++ adunare2nr/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace adunare2nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            { int a;
+              int b;
+                Console.Write("a= ");
+                a = int.Parse(Console.ReadLine());
+                //a = Int32.MaxValue;  check for error
+                Console.Write("b= ");
+                b = int.Parse(Console.ReadLine());
+
+                if (Int32.MaxValue - a <=b)
+                {
+                   
+                    Console.WriteLine("overflow!");
+
+                }
+                else 
+                {
+                    Console.WriteLine("Suma numerelor = " + (a + b));
+                }
+               
+            }
+        }
+    }
+}
